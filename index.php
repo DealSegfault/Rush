6585bede6b4d73feff56af21d24baa0f3f06b0bb
Michael HALIT <mhalit@e2r12p13.42.fr>
2017-04-08 15:04:10 +0200

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <header class="header"><h1>Piscine 42</h1><img src="resources/header.jpg"></header>
+    <body>
+        <ul class="sidenav">
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#news">Login</a></li>
+          <li><a href="#contact">My Account</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+        <div class="content">
+            ..HOME
+        </div>
+    </body>
+</html>
