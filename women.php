64d1c2bbbdae3459f4545711fdc0541c0fdc754a
Antonin GAVREL <angavrel@e2r10p6.42.fr>
2017-04-09 13:49:16 +0200

    go

--- women.php
+++ women.php
@@ -26,29 +26,6 @@ function		connect()
 
 <?php include ("incl/left_menu.php"); ?>
 
-<div class="main">
-<?php 
-
-    $link = connect(); 
-    $result = mysqli_query($link, "SELECT * FROM products WHERE category = 'women'");
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-    {
-    ?>
-        <form method="post" action="<?= $new_url_get ?>">
-        <li>
-            <div style="display:inline-block;">
-                <div><img width="290px" height="400px" src="<?= $row['img_url']; ?>"></div>
-                <div><span><?= $row['title']; ?></span></div>
-                <div><span><?= $row['price']; ?>€</span></div>
-                <div><input type="hidden" name="id" value="<?= $row['id_product']; ?>"></div>
-                <div><input type="hidden" name="price" value="<?= $row['price']; ?>"></div>
-                <div><input class="button" type="submit" name="submit" value="Add to cart"></div>
-            </div>
-        </li>
-        </form>
-    <?php 
-    }
-    ?>
-</div>
 </body>
 </html>
+
